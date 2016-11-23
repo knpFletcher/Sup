20d06f22c0f72b155f3db0fe178ecfa5816798e4
pownallk <pownallk@gmail.com>
2016-11-23 10:32:04 -0600

    work from lessons: "Grids" and "Separating the Layout"

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,8 +6,11 @@ import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -15,21 +18,30 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        Group root = new Group();
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+       /* Group root = new Group();
 
         Text txt = new Text("Sup?");
+        txt.setFont(new Font("Papyrus", 80));
+        Label label = new Label("Name");
         TextField nameFld = new TextField();
 
         Button btn = new Button();
         btn.setText("Say Sup!");
         btn.setOnAction(evt -> System.out.printf("Sup %s!%n", nameFld.getText()));
 
+        GridPane grid = new GridPane();
+        //grid.setGridLinesVisible(true);
+        grid.add(label, 0, 0); //zero based, like array
+        grid.add(nameFld, 1, 0);
+        grid.add(btn, 1, 1);
+        grid.setHgap(20);
+
         txt.setY(50);
 
         VBox box = new VBox();
-        box.getChildren().addAll(txt, nameFld, btn);
-        root.getChildren().add(box);
+        box.getChildren().addAll(txt, grid);
+        root.getChildren().add(box);*/
 
         primaryStage.setTitle("Sup");
         primaryStage.setScene(new Scene(root, 300, 275));
